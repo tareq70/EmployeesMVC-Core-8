6d23e633daed34e67f2a81c54c5f7e0d7a3edc3b
Tareq Elsabagh <144366153+tareq70@users.noreply.github.com>
2026-02-10 13:12:26 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeesMVC_Core_8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
